66885b63bc5816ce6ce065a5a87350009da2ec56
Jales Nunes <jales.nunes@hotmail.com>
2023-10-16 15:26:50 -0600

    search bar and transaction table added

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { Globalstyle } from './styles/global'
 import { Transactions } from './pages/Transactions'
-import { Summary } from './components/Summary'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Globalstyle />
       <Transactions />
-      <Summary />
     </ThemeProvider>
   )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,43 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <SearchForm />
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">$ 12,000.00</PriceHighlight>
+              </td>
+
+              <td>Venda</td>
+              <td>04/13/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Pizza</td>
+              <td>
+                <PriceHighlight variant="outcome">- $ 12.00</PriceHighlight>
+              </td>
+              <td>Alimentacao</td>
+              <td>04/14/2023</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
