b0e69dff5b4686b8de50c0ac128ebe294fbdbbe2
Jales Nunes <jales.nunes@hotmail.com>
2023-11-22 12:26:59 -0700

    added some features to better app performance

--- src/hooks/useSummary.tsx
+++ src/hooks/useSummary.tsx
@@ -1,23 +1,28 @@
-import { useContext } from 'react'
+import { useMemo } from 'react'
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
 
 export function useSummary() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.amount
-        acc.total += transaction.amount
-      } else {
-        acc.outcome += transaction.amount
-        acc.total -= transaction.amount
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.amount
+          acc.total += transaction.amount
+        } else {
+          acc.outcome += transaction.amount
+          acc.total -= transaction.amount
+        }
 
-      return acc
-    },
-    { income: 0, outcome: 0, total: 0 },
-  )
+        return acc
+      },
+      { income: 0, outcome: 0, total: 0 },
+    )
+  }, [transactions])
 
   return summary
 }
